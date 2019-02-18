412be64a540de978e2f9ffb6c4838af28df85f3f
arno <arnaud.lacour@gmail.com>
2019-02-17 20:30:28 -0500

    cleanup and add hooks

---
+++ hooks/00-immediate-startup.sh
@@ -0,0 +1,2 @@
+#!/bin/env sh
+echo "This happens on every start of the container, immediately after the container starts"
---
+++ hooks/50-before-post-start.sh
@@ -0,0 +1,2 @@
+#!/bin/env sh
+echo "This happens on every start of the container, immediately before the post-start script is started in the background"
