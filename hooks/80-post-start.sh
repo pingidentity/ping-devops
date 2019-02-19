326aa94733ae5e7f9a7cb6a7c3863b0579d48b86
arno <arnaud.lacour@gmail.com>
2019-02-19 06:34:36 -0500

    fixed small typo in hook stub

--- hooks/80-post-start.sh
+++ hooks/80-post-start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-echo "This can be used to replace the build-in, default post-start script which takes care of establishing replication."
+echo "This can be used to replace the built-in, default post-start script which takes care of establishing replication."
